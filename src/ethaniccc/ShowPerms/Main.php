d75f1778105e143b68b9aed91a99d4ebeee2fd3e
mmm545 <64874108+mmm545@users.noreply.github.com>
2022-01-08 17:06:51 +0300

    updated to pm4 and changed lots of stuff

--- src/ethaniccc/ShowPerms/Main.php
+++ src/ethaniccc/ShowPerms/Main.php
@@ -4,42 +4,23 @@ declare(strict_types=1);
 
 namespace ethaniccc\ShowPerms;
 
+use pocketmine\permission\PermissionManager;
 use pocketmine\plugin\PluginBase;
-use pocketmine\Server;
-use pocketmine\Player;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase{
 
-    private $perms = array();
-
-    public function onEnable(){
-        $perms = $this->getServer()->getPluginManager()->getPermissions();
-        $this->perms = $perms;
-    }
-
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-        switch($command->getName()){
-            case "showperms":
-                $perms = $this->perms;
-                if($sender instanceof Player){
-                    $sender->sendMessage(TextFormat::RED . "You must run this command through console!");
-                } else {
-                    $this->getLogger()->info(TextFormat::GREEN . "Here are all of the permission nodes: ");
-                    $permArray = $this->getServer()->getPluginManager()->getPermissions(); //Gets all permissions objects
-                    $permName_DescArray = NULL;
-                    $INDEX = 0;
-                    foreach($permArray as $perm)
-                    {
-                        $permName_DescArray[$INDEX] = TextFormat::DARK_PURPLE."Permission: ".TextFormat::GREEN.$perm->getName().TextFormat::YELLOW." Description: ".$perm->getDescription(); //Message parsing
-                        $INDEX++;
-                    }
-                    $this->getServer()->getLogger()->info(implode(" \n ",$permName_DescArray));
-                }
-            break;
+        // No 'if' or 'switch' statement needed here, we only have one command
+        $permArray = PermissionManager::getInstance()->getPermissions(); //Gets all permissions objects
+        $permName_DescArray = [];
+        foreach($permArray as $perm)
+        {
+            $permName_DescArray[] = TextFormat::DARK_PURPLE."Permission: ".TextFormat::GREEN.$perm->getName().TextFormat::YELLOW." Description: ".$perm->getDescription(); //Message parsing
         }
+        $this->getServer()->getLogger()->info("Here are all of the permission nodes:\n" . implode("\n",$permName_DescArray));
         return true;
     }
 
